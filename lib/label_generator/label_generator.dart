cff9b7a34ece86084ed8639da13a6059cb0994b3
Alex Olan <git.alex@yahoo.com>
2024-11-08 18:14:12 +0200

    added onPrint

--- lib/label_generator/label_generator.dart
+++ lib/label_generator/label_generator.dart
@@ -1,22 +1,21 @@
 import 'dart:typed_data';
-import 'label_printer_windows.dart'
-    if (kIsWeb) 'package:excelprinttest/label_generator/label_printer_html.dart';
+import 'label_printer_windows.dart' if (kIsWeb) 'package:excelprinttest/label_generator/label_printer_html.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:pdf/pdf.dart';
 
-
-
-class LabelGenerator{
-
+class LabelGenerator {
   static late PdfPageFormat pageFormat;
 
-  void setPageFormat(PdfPageFormat pageFormat){
+  void setPageFormat(PdfPageFormat pageFormat) {
     LabelGenerator.pageFormat = pageFormat;
   }
 
   Future<void> generateLabelPdf({required pw.Document pdf, required Function(Uint8List bytes)? onPrint}) async {
     final Uint8List pdfBytes = await pdf.save();
-    onPrint?.call(pdfBytes);
-    printPdf(pdfBytes);
+    if (onPrint != null) {
+      onPrint.call(pdfBytes);
+    } else {
+      printPdf(pdfBytes);
+    }
   }
 }
